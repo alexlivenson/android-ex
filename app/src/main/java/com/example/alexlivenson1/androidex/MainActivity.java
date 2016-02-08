aed5fd4d415086a09460404ddc32ccaffdb66827
alex livenson <alexlivenson1@alexs-MacBook-Pro.local>
2016-02-07 16:53:22 -0800

    Initial commit

---
+++ app/src/main/java/com/example/alexlivenson1/androidex/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.alexlivenson1.androidex;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    String fortuneList[] = {
+            "Don't count on it",
+            "Ask again later",
+            "You may rely on it",
+            "Without a doubt",
+            "Outlook not so good",
+            "It's decidedly so",
+            "Signs point to yes",
+            "Yes definitely",
+            "Yes",
+            "My sources say NO",
+    };
+
+    TextView mFortuneText;
+    Button mGenerateFortuneButton;
+    ImageView mFortuneBallImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // 1. Call super class to make sure activity ready to go
+        super.onCreate(savedInstanceState);
+
+        // 2. Specify the layout for this activity is provided by the layout you created before
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // 3. Populate values of the three members you created before for the views (id same as in XML layout)
+        mFortuneText = (TextView) findViewById(R.id.fortuneText);
+        mFortuneBallImage = (ImageView) findViewById(R.id.fortunateImage);
+        mGenerateFortuneButton = (Button) findViewById(R.id.fortuneButton);
+
+        // 4. Simple class encapsulates functionality you'd like to perform when button pressed
+        mGenerateFortuneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 5.
+                int index = new Random().nextInt(fortuneList.length);
+                mFortuneText.setText(fortuneList[index]);
+
+                // 6. 3rd party animation
+                YoYo.with(Techniques.Swing)
+                        .duration(500)
+                        .playOn(mFortuneBallImage);
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
